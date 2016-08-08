09c72936a1269c758d34aee55baff03ceac8b7b7
SHEFFcode <sheff@sheffmachine.com>
2016-08-07 19:27:19 -0700

    add form to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {FormsComponentComponent} from "./forms-component.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [FormsComponentComponent]
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   moduleId: module.id,
   selector: 'app-template-driven',
   template: `
     <h1>Template Driven</h1>
-    <form>
+    <form (ngSubmit)="onSubmit(f)" #f="ngForm">
       <div>
         <div class="form-group">
           <label for="username">Username</label>
@@ -13,12 +14,12 @@ import { Component, OnInit } from '@angular/core';
         </div>
         <div class="form-group">
           <label for="email">Email</label>
-          <input type="text" class="form-control" id="email" ngModel name="username">
+          <input type="text" class="form-control" id="email" ngModel name="email">
         </div>
       </div>
       <div class="form-group">
         <label for="password" >Password</label>
-        <input type="password" class="form-control" id="password" ngModel name="username">
+        <input type="password" class="form-control" id="password" ngModel name="password">
       </div>
       <div class="radio">
         <label></label>
@@ -33,6 +34,10 @@ export class TemplateDrivenComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 
 }
