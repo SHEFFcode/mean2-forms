e009552476ce6f6b36983307f80991894b6acabf
SHEFFcode <sheff@sheffmachine.com>
2016-08-12 18:43:07 -0700

    play around with forms some more, they seem to be broken in RC4 at the moment

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,20 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, REACTIVE_FORM_DIRECTIVES} from "@angular/forms";
 
 @Component({
   moduleId: module.id,
   selector: 'app-data-driven',
   template: `
-    <p>
-      data-driven Works!
-    </p>
+    <h1>Data Driven</h1>
+    <form [formGroup]="myForm" (ngSubmit)="onSubmit()">
+      <div>
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" class="form-control" id="username" formControlName="username">
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" class="form-control" id="email" formControlName="email">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="password" >Password</label>
+        <input type="password" class="form-control" id="password" formControlName="password">
+      </div>
+      <div class="radio">
+        <label>
+          <input type="radio" name="gender">
+        </label>
+      </div>
+      <div>
+        <h3>Hobbies</h3>
+        <div class="form-group">
+        <input type="text" class="form-control">
+        </div>
+      </div>
+      <button type="submit" class="btn btn-default">Add Hobby</button>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   `,
-  styles: []
+  styles: [],
+  directives: [REACTIVE_FORM_DIRECTIVES]
 })
 export class DataDrivenComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+
+  constructor() {
+    this.myForm = new FormGroup({
+      username:   new FormControl(),
+      email:      new FormControl(),
+      password:   new FormControl()
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.myForm);
   }
 
 }
--- src/app/forms-component.component.ts
+++ src/app/forms-component.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {TemplateDrivenComponent} from "./template-driven/template-driven.component";
+import {DataDrivenComponent} from "./data-driven/data-driven.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-forms-component',
   templateUrl: 'forms-component.component.html',
   styles: [],
-  directives: [TemplateDrivenComponent]
+  directives: [DataDrivenComponent]
 })
 export class FormsComponentComponent implements OnInit {
 
